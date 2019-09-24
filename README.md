f9c5b7f93aa3099c879d0d6ad8ffa429e90c0497
Alberto <155007+Veilkrand@users.noreply.github.com>
2019-09-24 21:06:47 +0900

    Updates.

--- README.md
+++ README.md
@@ -29,17 +29,3 @@ MIT
 
 * for PXT/microbit
 (The metadata above is needed for package search.)
-
----
-
-## TODO
-
-- [X] Add a reference for your blocks here
-- [X] Add "icon.png" image (300x200) in the root folder
-- [X] Add "- beta" to the GitHub project description if you are still iterating it.
-- [X] Turn on your automated build on https://travis-ci.org
-- [X] Use "pxt bump" to create a tagged release on GitHub
-- [X] On GitHub, create a new file named LICENSE. Select the MIT License template.
-- [ ] Get your package reviewed and approved https://makecode.microbit.org/extensions/approval
-
-Read more at https://makecode.microbit.org/extensions
--- test.ts
+++ test.ts
@@ -1,1 +1,42 @@
-// tests go here; this will not be compiled when this package is used as a library
+{
+
+    // Show leds colors and spin for 2 secs
+    RobotCar_Keyestudio.Leds.showRed()
+    RobotCar_Keyestudio.Motors.spin(50)
+    basic.pause(2000)
+    RobotCar_Keyestudio.Leds.setRGB(255, 255, 0)
+    basic.pause(1000)
+    RobotCar_Keyestudio.Leds.showGreen()
+    basic.pause(1000)
+    RobotCar_Keyestudio.Leds.showBlue()
+    basic.pause(1000)
+    RobotCar_Keyestudio.Leds.showWhite()
+
+    /** 
+     * Keep distance and avoid obstacle
+     * 
+     **/
+    while (true) {
+        // Keep distance to obstacle
+        let distance = RobotCar_Keyestudio.Sonar.ping()
+        if (distance > 10) {
+            RobotCar_Keyestudio.Motors.move(50)
+
+            // Check IR sensors
+            if (RobotCar_Keyestudio.IrSensors.isLeftBlocked()) {
+                RobotCar_Keyestudio.Motors.steer(30, 50)
+            } else if (RobotCar_Keyestudio.IrSensors.isRightBlocked()) {
+                RobotCar_Keyestudio.Motors.steer(-30, 50)
+            }
+
+        } else if (distance < 8) {
+            RobotCar_Keyestudio.Motors.move(-50)
+        } else {
+            RobotCar_Keyestudio.Motors.stop()
+        }
+
+
+    }
+
+
+}
